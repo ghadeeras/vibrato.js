9a3be2981a397ba91dfdc7266d3d1ccac8fb6954
Ghadeer <ghadeer.abousaleh@gmail.com>
2021-03-15 00:04:32 -0400

    Adding basic low-level vector math operations

--- src/prod/rt.ts
+++ src/prod/rt.ts
@@ -28,6 +28,70 @@ export type SpaceExports = {
     f64_vec_add: (size: number, v1: Reference, v2: Reference) => Reference;
     f64_vec_add_r: (size: number, v1: Reference, v2: Reference, result: Reference) => Reference;
 
+    f64_vec2_sub: (v1: Reference, v2: Reference) => Reference;
+    f64_vec2_sub_r: (v1: Reference, v2: Reference, result: Reference) => Reference;
+    f64_vec3_sub: (v1: Reference, v2: Reference) => Reference;
+    f64_vec3_sub_r: (v1: Reference, v2: Reference, result: Reference) => Reference;
+    f64_vec4_sub: (v1: Reference, v2: Reference) => Reference;
+    f64_vec4_sub_r: (v1: Reference, v2: Reference, result: Reference) => Reference;
+    f64_vec_sub: (size: number, v1: Reference, v2: Reference) => Reference;
+    f64_vec_sub_r: (size: number, v1: Reference, v2: Reference, result: Reference) => Reference;
+
+    f64_vec2_mul: (v1: Reference, v2: Reference) => Reference;
+    f64_vec2_mul_r: (v1: Reference, v2: Reference, result: Reference) => Reference;
+    f64_vec3_mul: (v1: Reference, v2: Reference) => Reference;
+    f64_vec3_mul_r: (v1: Reference, v2: Reference, result: Reference) => Reference;
+    f64_vec4_mul: (v1: Reference, v2: Reference) => Reference;
+    f64_vec4_mul_r: (v1: Reference, v2: Reference, result: Reference) => Reference;
+    f64_vec_mul: (size: number, v1: Reference, v2: Reference) => Reference;
+    f64_vec_mul_r: (size: number, v1: Reference, v2: Reference, result: Reference) => Reference;
+
+    f64_vec2_div: (v1: Reference, v2: Reference) => Reference;
+    f64_vec2_div_r: (v1: Reference, v2: Reference, result: Reference) => Reference;
+    f64_vec3_div: (v1: Reference, v2: Reference) => Reference;
+    f64_vec3_div_r: (v1: Reference, v2: Reference, result: Reference) => Reference;
+    f64_vec4_div: (v1: Reference, v2: Reference) => Reference;
+    f64_vec4_div_r: (v1: Reference, v2: Reference, result: Reference) => Reference;
+    f64_vec_div: (size: number, v1: Reference, v2: Reference) => Reference;
+    f64_vec_div_r: (size: number, v1: Reference, v2: Reference, result: Reference) => Reference;
+
+    f64_vec2_scalar_mul: (v: Reference, s: number) => Reference;
+    f64_vec2_scalar_mul_r: (v: Reference, s: number, result: Reference) => Reference;
+    f64_vec3_scalar_mul: (v: Reference, s: number) => Reference;
+    f64_vec3_scalar_mul_r: (v: Reference, s: number, result: Reference) => Reference;
+    f64_vec4_scalar_mul: (v: Reference, s: number) => Reference;
+    f64_vec4_scalar_mul_r: (v: Reference, s: number, result: Reference) => Reference;
+    f64_vec_scalar_mul: (size: number, v: Reference, s: number) => Reference;
+    f64_vec_scalar_mul_r: (size: number, v: Reference, s: number, result: Reference) => Reference;
+
+    f64_vec2_scalar_div: (v: Reference, s: number) => Reference;
+    f64_vec2_scalar_div_r: (v: Reference, s: number, result: Reference) => Reference;
+    f64_vec3_scalar_div: (v: Reference, s: number) => Reference;
+    f64_vec3_scalar_div_r: (v: Reference, s: number, result: Reference) => Reference;
+    f64_vec4_scalar_div: (v: Reference, s: number) => Reference;
+    f64_vec4_scalar_div_r: (v: Reference, s: number, result: Reference) => Reference;
+    f64_vec_scalar_div: (size: number, v: Reference, s: number) => Reference;
+    f64_vec_scalar_div_r: (size: number, v: Reference, s: number, result: Reference) => Reference;
+
+    f64_vec2_dot: (v1: Reference, v2: Reference) => number;
+    f64_vec3_dot: (v1: Reference, v2: Reference) => number;
+    f64_vec4_dot: (v1: Reference, v2: Reference) => number;
+    f64_vec_dot: (size: number, v1: Reference, v2: Reference) => number;
+
+    f64_vec2_length: (v: Reference) => number;
+    f64_vec3_length: (v: Reference) => number;
+    f64_vec4_length: (v: Reference) => number;
+    f64_vec_length: (size: number, v: Reference) => number;
+
+    f64_vec2_normalize: (v: Reference) => Reference;
+    f64_vec2_normalize_r: (v: Reference, result: Reference) => Reference;
+    f64_vec3_normalize: (v: Reference) => Reference;
+    f64_vec3_normalize_r: (v: Reference, result: Reference) => Reference;
+    f64_vec4_normalize: (v: Reference) => Reference;
+    f64_vec4_normalize_r: (v: Reference, result: Reference) => Reference;
+    f64_vec_normalize: (size: number, v: Reference) => Reference;
+    f64_vec_normalize_r: (size: number, v: Reference, result: Reference) => Reference;
+
 }
 
 export function addImportsToModule(module: binaryen.Module) {
@@ -46,16 +110,85 @@ export function addMemImportsToModule(module: binaryen.Module) {
 }
 
 export function addSpaceImportsToModule(module: binaryen.Module) {
-    const fOp = binaryen.createType([binaryen.i32, binaryen.i32]);
-    const pOp = binaryen.createType([binaryen.i32, binaryen.i32, binaryen.i32]);
-    module.addFunctionImport("f64_vec2_add", "space", "f64_vec2_add", fOp, binaryen.i32)
-    module.addFunctionImport("f64_vec2_add_r", "space", "f64_vec2_add_r", pOp, binaryen.i32)
-    module.addFunctionImport("f64_vec3_add", "space", "f64_vec3_add", fOp, binaryen.i32)
-    module.addFunctionImport("f64_vec3_add_r", "space", "f64_vec3_add_r", pOp, binaryen.i32)
-    module.addFunctionImport("f64_vec4_add", "space", "f64_vec4_add", fOp, binaryen.i32)
-    module.addFunctionImport("f64_vec4_add_r", "space", "f64_vec4_add_r", pOp, binaryen.i32)
-    module.addFunctionImport("f64_vec_add", "space", "f64_vec_add", fOp, binaryen.i32)
-    module.addFunctionImport("f64_vec_add_r", "space", "f64_vec_add_r", pOp, binaryen.i32)
+    const vec1_vec2 = binaryen.createType([binaryen.i32, binaryen.i32]);
+    const vec1_vec2_result = binaryen.createType([binaryen.i32, binaryen.i32, binaryen.i32]);
+    const vec_scalar = binaryen.createType([binaryen.i32, binaryen.f64]);
+    const vec_scalar_result = binaryen.createType([binaryen.i32, binaryen.f64, binaryen.i32]);
+    const vec = binaryen.createType([binaryen.i32]);
+    const vec_result = binaryen.createType([binaryen.i32, binaryen.i32]);
+
+    module.addFunctionImport("f64_vec2_add", "space", "f64_vec2_add", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec2_add_r", "space", "f64_vec2_add_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec3_add", "space", "f64_vec3_add", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec3_add_r", "space", "f64_vec3_add_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec4_add", "space", "f64_vec4_add", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec4_add_r", "space", "f64_vec4_add_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec_add", "space", "f64_vec_add", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec_add_r", "space", "f64_vec_add_r", vec1_vec2_result, binaryen.i32)
+
+    module.addFunctionImport("f64_vec2_sub", "space", "f64_vec2_sub", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec2_sub_r", "space", "f64_vec2_sub_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec3_sub", "space", "f64_vec3_sub", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec3_sub_r", "space", "f64_vec3_sub_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec4_sub", "space", "f64_vec4_sub", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec4_sub_r", "space", "f64_vec4_sub_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec_sub", "space", "f64_vec_sub", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec_sub_r", "space", "f64_vec_sub_r", vec1_vec2_result, binaryen.i32)
+
+    module.addFunctionImport("f64_vec2_mul", "space", "f64_vec2_mul", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec2_mul_r", "space", "f64_vec2_mul_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec3_mul", "space", "f64_vec3_mul", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec3_mul_r", "space", "f64_vec3_mul_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec4_mul", "space", "f64_vec4_mul", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec4_mul_r", "space", "f64_vec4_mul_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec_mul", "space", "f64_vec_mul", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec_mul_r", "space", "f64_vec_mul_r", vec1_vec2_result, binaryen.i32)
+
+    module.addFunctionImport("f64_vec2_div", "space", "f64_vec2_div", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec2_div_r", "space", "f64_vec2_div_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec3_div", "space", "f64_vec3_div", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec3_div_r", "space", "f64_vec3_div_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec4_div", "space", "f64_vec4_div", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec4_div_r", "space", "f64_vec4_div_r", vec1_vec2_result, binaryen.i32)
+    module.addFunctionImport("f64_vec_div", "space", "f64_vec_div", vec1_vec2, binaryen.i32)
+    module.addFunctionImport("f64_vec_div_r", "space", "f64_vec_div_r", vec1_vec2_result, binaryen.i32)
+
+    module.addFunctionImport("f64_vec2_scalar_mul", "space", "f64_vec2_scalar_mul", vec_scalar, binaryen.i32)
+    module.addFunctionImport("f64_vec2_scalar_mul_r", "space", "f64_vec2_scalar_mul_r", vec_scalar_result, binaryen.i32)
+    module.addFunctionImport("f64_vec3_scalar_mul", "space", "f64_vec3_scalar_mul", vec_scalar, binaryen.i32)
+    module.addFunctionImport("f64_vec3_scalar_mul_r", "space", "f64_vec3_scalar_mul_r", vec_scalar_result, binaryen.i32)
+    module.addFunctionImport("f64_vec4_scalar_mul", "space", "f64_vec4_scalar_mul", vec_scalar, binaryen.i32)
+    module.addFunctionImport("f64_vec4_scalar_mul_r", "space", "f64_vec4_scalar_mul_r", vec_scalar_result, binaryen.i32)
+    module.addFunctionImport("f64_vec_scalar_mul", "space", "f64_vec_scalar_mul", vec_scalar, binaryen.i32)
+    module.addFunctionImport("f64_vec_scalar_mul_r", "space", "f64_vec_scalar_mul_r", vec_scalar_result, binaryen.i32)
+
+    module.addFunctionImport("f64_vec2_scalar_div", "space", "f64_vec2_scalar_div", vec_scalar, binaryen.i32)
+    module.addFunctionImport("f64_vec2_scalar_div_r", "space", "f64_vec2_scalar_div_r", vec_scalar_result, binaryen.i32)
+    module.addFunctionImport("f64_vec3_scalar_div", "space", "f64_vec3_scalar_div", vec_scalar, binaryen.i32)
+    module.addFunctionImport("f64_vec3_scalar_div_r", "space", "f64_vec3_scalar_div_r", vec_scalar_result, binaryen.i32)
+    module.addFunctionImport("f64_vec4_scalar_div", "space", "f64_vec4_scalar_div", vec_scalar, binaryen.i32)
+    module.addFunctionImport("f64_vec4_scalar_div_r", "space", "f64_vec4_scalar_div_r", vec_scalar_result, binaryen.i32)
+    module.addFunctionImport("f64_vec_scalar_div", "space", "f64_vec_scalar_div", vec_scalar, binaryen.i32)
+    module.addFunctionImport("f64_vec_scalar_div_r", "space", "f64_vec_scalar_div_r", vec_scalar_result, binaryen.i32)
+
+    module.addFunctionImport("f64_vec2_dot", "space", "f64_vec2_dot", vec1_vec2, binaryen.f64)
+    module.addFunctionImport("f64_vec3_dot", "space", "f64_vec3_dot", vec1_vec2, binaryen.f64)
+    module.addFunctionImport("f64_vec4_dot", "space", "f64_vec4_dot", vec1_vec2, binaryen.f64)
+    module.addFunctionImport("f64_vec_dot", "space", "f64_vec_dot", vec1_vec2, binaryen.f64)
+
+    module.addFunctionImport("f64_vec2_length", "space", "f64_vec2_dot", vec, binaryen.f64)
+    module.addFunctionImport("f64_vec3_length", "space", "f64_vec3_dot", vec, binaryen.f64)
+    module.addFunctionImport("f64_vec4_length", "space", "f64_vec4_dot", vec, binaryen.f64)
+    module.addFunctionImport("f64_vec_length", "space", "f64_vec_dot", vec, binaryen.f64)
+
+    module.addFunctionImport("f64_vec2_normalize", "space", "f64_vec2_normalize", vec, binaryen.i32)
+    module.addFunctionImport("f64_vec2_normalize_r", "space", "f64_vec2_normalize_r", vec_result, binaryen.i32)
+    module.addFunctionImport("f64_vec3_normalize", "space", "f64_vec3_normalize", vec, binaryen.i32)
+    module.addFunctionImport("f64_vec3_normalize_r", "space", "f64_vec3_normalize_r", vec_result, binaryen.i32)
+    module.addFunctionImport("f64_vec4_normalize", "space", "f64_vec4_normalize", vec, binaryen.i32)
+    module.addFunctionImport("f64_vec4_normalize_r", "space", "f64_vec4_normalize_r", vec_result, binaryen.i32)
+    module.addFunctionImport("f64_vec_normalize", "space", "f64_vec_normalize", vec, binaryen.i32)
+    module.addFunctionImport("f64_vec_normalize_r", "space", "f64_vec_normalize_r", vec_result, binaryen.i32)
 }
 
 const modules = {
--- src/test/rt.test.ts
+++ src/test/rt.test.ts
@@ -103,7 +103,9 @@ describe("Runtime", () => {
         const vector4D = dt.vectorOf(4, dt.real)
         const vectorND = dt.vectorOf(randomInt(5, 64), dt.real)
 
-        describe("add", () => {
+        const notZero = (n: number): boolean => n != 0
+
+        describe("real vector add", () => {
 
             it("adds 2D real vector to another", () => {
                 const [ref1, v1] = randomVector(vector2D)
@@ -147,23 +149,379 @@ describe("Runtime", () => {
 
         })
 
+        describe("real vector sub", () => {
+
+            it("subtracts 2D real vector from another", () => {
+                const [ref1, v1] = randomVector(vector2D)
+                const [ref2, v2] = randomVector(vector2D)
+    
+                const ref = space.f64_vec2_sub(ref1, ref2)
+                const vector = vector2D.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] - v2[i]))
+            })
+
+            it("subtracts 3D real vector from another", () => {
+                const [ref1, v1] = randomVector(vector3D)
+                const [ref2, v2] = randomVector(vector3D)
+    
+                const ref = space.f64_vec3_sub(ref1, ref2)
+                const vector = vector3D.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] - v2[i]))
+            })
+
+            it("subtracts 4D real vector from another", () => {
+                const [ref1, v1] = randomVector(vector4D)
+                const [ref2, v2] = randomVector(vector4D)
+    
+                const ref = space.f64_vec4_sub(ref1, ref2)
+                const vector = vector4D.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] - v2[i]))
+            })
+
+            it("subtracts ND real vector from another", () => {
+                const [ref1, v1] = randomVector(vectorND)
+                const [ref2, v2] = randomVector(vectorND)
+    
+                const ref = space.f64_vec_sub(vectorND.size, ref1, ref2)
+                const vector = vectorND.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] - v2[i]))
+            })
+
+        })
+
+        describe("real vector mul", () => {
+
+            it("multiplies 2D real vector with another", () => {
+                const [ref1, v1] = randomVector(vector2D)
+                const [ref2, v2] = randomVector(vector2D)
+    
+                const ref = space.f64_vec2_mul(ref1, ref2)
+                const vector = vector2D.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] * v2[i]))
+            })
+
+            it("multiplies 3D real vector with another", () => {
+                const [ref1, v1] = randomVector(vector3D)
+                const [ref2, v2] = randomVector(vector3D)
+    
+                const ref = space.f64_vec3_mul(ref1, ref2)
+                const vector = vector3D.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] * v2[i]))
+            })
+
+            it("multiplies 4D real vector with another", () => {
+                const [ref1, v1] = randomVector(vector4D)
+                const [ref2, v2] = randomVector(vector4D)
+    
+                const ref = space.f64_vec4_mul(ref1, ref2)
+                const vector = vector4D.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] * v2[i]))
+            })
+
+            it("multiplies ND real vector with another", () => {
+                const [ref1, v1] = randomVector(vectorND)
+                const [ref2, v2] = randomVector(vectorND)
+    
+                const ref = space.f64_vec_mul(vectorND.size, ref1, ref2)
+                const vector = vectorND.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] * v2[i]))
+            })
+
+        })
+
+        describe("real vector div", () => {
+
+            it("divides 2D real vector by another", () => {
+                const [ref1, v1] = randomVector(vector2D)
+                const [ref2, v2] = randomVector(vector2D, notZero)
+    
+                const ref = space.f64_vec2_div(ref1, ref2)
+                const vector = vector2D.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] / v2[i]))
+            })
+
+            it("divides 3D real vector by another", () => {
+                const [ref1, v1] = randomVector(vector3D)
+                const [ref2, v2] = randomVector(vector3D, notZero)
+    
+                const ref = space.f64_vec3_div(ref1, ref2)
+                const vector = vector3D.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] / v2[i]))
+            })
+
+            it("divides 4D real vector by another", () => {
+                const [ref1, v1] = randomVector(vector4D)
+                const [ref2, v2] = randomVector(vector4D, notZero)
+    
+                const ref = space.f64_vec4_div(ref1, ref2)
+                const vector = vector4D.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] / v2[i]))
+            })
+
+            it("divides ND real vector by another", () => {
+                const [ref1, v1] = randomVector(vectorND)
+                const [ref2, v2] = randomVector(vectorND, notZero)
+    
+                const ref = space.f64_vec_div(vectorND.size, ref1, ref2)
+                const vector = vectorND.flatView(mem.stack.buffer, ref, 1)
+    
+                vector.forEach((v, i) => expect(v).to.equal(v1[i] / v2[i]))
+            })
+
+        })
+
+        describe("real vector scalar mul", () => {
+
+            it("multiplies 2D real vector with a scalar", () => {
+                const [inRef, inVec] = randomVector(vector2D)
+                const s = randomNumber()
+    
+                const outRef = space.f64_vec2_scalar_mul(inRef, s)
+                const outVec = vector2D.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] * s))
+            })
+
+            it("multiplies 3D real vector with a scalar", () => {
+                const [inRef, inVec] = randomVector(vector3D)
+                const s = randomNumber()
+    
+                const outRef = space.f64_vec3_scalar_mul(inRef, s)
+                const outVec = vector3D.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] * s))
+            })
+
+            it("multiplies 4D real vector with a scalar", () => {
+                const [inRef, inVec] = randomVector(vector4D)
+                const s = randomNumber()
+    
+                const outRef = space.f64_vec4_scalar_mul(inRef, s)
+                const outVec = vector4D.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] * s))
+            })
+
+            it("multiplies ND real vector with a scalar", () => {
+                const [inRef, inVec] = randomVector(vectorND)
+                const s = randomNumber()
+    
+                const outRef = space.f64_vec_scalar_mul(vectorND.size, inRef, s)
+                const outVec = vectorND.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] * s))
+            })
+
+        })
+
+        describe("real vector scalar div", () => {
+
+            it("divides 2D real vector by a scalar", () => {
+                const [inRef, inVec] = randomVector(vector2D)
+                const s = randomNumber(notZero)
+    
+                const outRef = space.f64_vec2_scalar_div(inRef, s)
+                const outVec = vector2D.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] / s))
+            })
+
+            it("divides 3D real vector by a scalar", () => {
+                const [inRef, inVec] = randomVector(vector3D)
+                const s = randomNumber(notZero)
+    
+                const outRef = space.f64_vec3_scalar_div(inRef, s)
+                const outVec = vector3D.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] / s))
+            })
+
+            it("divides 4D real vector by a scalar", () => {
+                const [inRef, inVec] = randomVector(vector4D)
+                const s = randomNumber(notZero)
+    
+                const outRef = space.f64_vec4_scalar_div(inRef, s)
+                const outVec = vector4D.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] / s))
+            })
+
+            it("divides ND real vector by a scalar", () => {
+                const [inRef, inVec] = randomVector(vectorND)
+                const s = randomNumber(notZero)
+    
+                const outRef = space.f64_vec_scalar_div(vectorND.size, inRef, s)
+                const outVec = vectorND.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] / s))
+            })
+
+        })
+
+        describe("real vector dot product", () => {
+
+            it("calculates dot product between 2D real vector and another", () => {
+                const [ref1, v1] = randomVector(vector2D)
+                const [ref2, v2] = randomVector(vector2D)
+    
+                const result = space.f64_vec2_dot(ref1, ref2)
+    
+                expect(result).to.equal(dot(v1, v2))
+            })
+
+            it("calculates dot product between 3D real vector and another", () => {
+                const [ref1, v1] = randomVector(vector3D)
+                const [ref2, v2] = randomVector(vector3D)
+    
+                const result = space.f64_vec3_dot(ref1, ref2)
+    
+                expect(result).to.equal(dot(v1, v2))
+            })
+
+            it("calculates dot product between 4D real vector and another", () => {
+                const [ref1, v1] = randomVector(vector4D)
+                const [ref2, v2] = randomVector(vector4D)
+    
+                const result = space.f64_vec4_dot(ref1, ref2)
+    
+                expect(result).to.equal(dot(v1, v2))
+            })
+
+            it("calculates dot product between ND real vector and another", () => {
+                const [ref1, v1] = randomVector(vectorND)
+                const [ref2, v2] = randomVector(vectorND)
+    
+                const result = space.f64_vec_dot(vectorND.size, ref1, ref2)
+    
+                expect(result).to.equal(dot(v1, v2))
+            })
+
+        })
+
+        describe("real vector length", () => {
+
+            it("calculates length of 2D real vector", () => {
+                const [ref, v] = randomVector(vector2D)
+    
+                const result = space.f64_vec2_length(ref)
+    
+                expect(result).to.equal(length(v))
+            })
+
+            it("calculates length of 3D real vector", () => {
+                const [ref, v] = randomVector(vector3D)
+    
+                const result = space.f64_vec3_length(ref)
+    
+                expect(result).to.equal(length(v))
+            })
+
+            it("calculates length of 4D real vector", () => {
+                const [ref, v] = randomVector(vector4D)
+    
+                const result = space.f64_vec4_length(ref)
+    
+                expect(result).to.equal(length(v))
+            })
+
+            it("calculates length of ND real vector", () => {
+                const [ref, v] = randomVector(vectorND)
+    
+                const result = space.f64_vec_length(vectorND.size, ref)
+    
+                expect(result).to.equal(length(v))
+            })
+
+        })
+
+        describe("real vector normalization", () => {
+
+            it("normalizes 2D real vector", () => {
+                const [inRef, inVec] = randomVector(vector2D, notZero)
+                const s = length(inVec)
+    
+                const outRef = space.f64_vec2_normalize(inRef)
+                const outVec = vector2D.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] / s))
+            })
+
+            it("normalizes 3D real vector", () => {
+                const [inRef, inVec] = randomVector(vector3D, notZero)
+                const s = length(inVec)
+    
+                const outRef = space.f64_vec3_normalize(inRef)
+                const outVec = vector3D.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] / s))
+            })
+
+            it("normalizes 4D real vector", () => {
+                const [inRef, inVec] = randomVector(vector4D, notZero)
+                const s = length(inVec)
+    
+                const outRef = space.f64_vec4_normalize(inRef)
+                const outVec = vector4D.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] / s))
+            })
+
+            it("normalizes ND real vector", () => {
+                const [inRef, inVec] = randomVector(vectorND, notZero)
+                const s = length(inVec)
+    
+                const outRef = space.f64_vec_normalize(vectorND.size, inRef)
+                const outVec = vectorND.flatView(mem.stack.buffer, outRef, 1)
+    
+                outVec.forEach((v, i) => expect(v).to.equal(inVec[i] / s))
+            })
+
+        })
+
     })
 
 })
 
 type Op = (v1: rt.Reference, v2: rt.Reference) => rt.Reference
 
-function randomVector(vectorType: dt.Vector<Float64Array>): [rt.Reference, Float64Array] {
+function length(v: Float64Array): number {
+    return Math.sqrt(dot(v, v))
+}
+
+function dot(v1: Float64Array, v2: Float64Array): number {
+    return v1.reduce((p, c1, i) => p + c1 * v2[i], 0)
+}
+
+function randomVector(vectorType: dt.Vector<Float64Array>, filter: (n: number) => boolean = () => true): [rt.Reference, Float64Array] {
     const ref = mem.allocate64(vectorType.size)
     const vector = vectorType.flatView(mem.stack.buffer, ref, 1)
-    randomize(vector)
+    randomize(vector, filter)
     return [ref, vector]
 }
 
-function randomize(a: Float64Array) {
+function randomize(a: Float64Array, filter: (n: number) => boolean = () => true) {
     for (let i = 0; i < a.length; i++) {
-        a[i] = Math.random()
+        a[i] = randomNumber(filter)
     }
+}
+
+function randomNumber(filter: (n: number) => boolean = () => true) {
+    let n = Math.random()
+    while (!filter(n)) {
+        n = Math.random()
+    }
+    return n
 }
 
 function randomInt(min: number = 0, max: number = min + 100): number {
